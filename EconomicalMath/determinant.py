50de7f30d6e12397c67de13f9474b11cd262ed08
notHawking <48393306+notHawking@users.noreply.github.com>
2023-09-06 12:01:41 +0000

    new function

---
+++ EconomicalMath/determinant.py
@@ -0,0 +1,16 @@
+"""Module determinant calculating determinant """
+
+baseArray3x3 = [[1.0, 2.0, 3.0],[4.0,5.0,6.0],[7.0,8.0,9.0]]
+result = float(0)
+
+#for i in range(len(baseArray3x3)) :
+#    for j in range(len(baseArray3x3[i])) :
+#        print(baseArray3x3[i][j])
+
+result1 = baseArray3x3[0][0]*baseArray3x3[1][1]*baseArray3x3[2][2]
+print("Step 1: {0,0 * 1,1 * 2,2} :" , baseArray3x3[0][0]  , " * " , baseArray3x3[1][1] , " * " , baseArray3x3[2][2], "=", result1)
+result2 = baseArray3x3[0][1]*baseArray3x3[1][2]*baseArray3x3[2][0]
+print("Step 2: {0,1 * 1,2 * 2,0} :" , baseArray3x3[0][1]  , " * " , baseArray3x3[1][2] , " * " , baseArray3x3[2][0], "=", result2)
+result3 = baseArray3x3[0][2]*baseArray3x3[1][0]*baseArray3x3[2][1]
+print("Step 3: {0,2 * 1,0 * 2,1} :" , baseArray3x3[0][2]  , " * " , baseArray3x3[1][0] , " * " , baseArray3x3[2][1], "=", result3)
+
