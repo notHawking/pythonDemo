302825ae3f521cc1521194da300dcb80ed6773be
notHawking <48393306+notHawking@users.noreply.github.com>
2023-09-06 12:21:24 +0000

    3x3 works

--- EconomicalMath/determinant.py
+++ EconomicalMath/determinant.py
@@ -14,3 +14,11 @@ print("Step 2: {0,1 * 1,2 * 2,0} :" , baseArray3x3[0][1]  , " * " , baseArray3x3
 result3 = baseArray3x3[0][2]*baseArray3x3[1][0]*baseArray3x3[2][1]
 print("Step 3: {0,2 * 1,0 * 2,1} :" , baseArray3x3[0][2]  , " * " , baseArray3x3[1][0] , " * " , baseArray3x3[2][1], "=", result3)
 
+result4 = baseArray3x3[0][2]*baseArray3x3[1][1]*baseArray3x3[2][0]
+print("Step 4: {0,2 * 1,1 * 2,0} :" , baseArray3x3[0][2]  , " * " , baseArray3x3[1][1] , " * " , baseArray3x3[2][0], "=", result4)
+result5 = baseArray3x3[0][1]*baseArray3x3[1][0]*baseArray3x3[2][2]
+print("Step 5: {0,1 * 1,0 * 2,0} :" , baseArray3x3[0][1]  , " * " , baseArray3x3[1][0] , " * " , baseArray3x3[2][2], "=", result5)
+result6 = baseArray3x3[0][0]*baseArray3x3[1][2]*baseArray3x3[2][1]
+print("Step 6: {0,0 * 1,2 * 2,1} :" , baseArray3x3[0][0]  , " * " , baseArray3x3[1][2] , " * " , baseArray3x3[2][1], "=", result6)
+finalResult = result1+result2+result3-result4-result5-result6
+print("Final step: {",result1,"+",result2,"+",result3,"-",result4,"-",result5,"-",result6,"} :",finalResult)
